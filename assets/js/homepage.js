9b186574073161eb08fab55d807ac60c6aeef4d3
HarishTeens <arishh2@gmail.com>
2018-04-06 01:18:17 +0530

    complete MENU

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -15,4 +15,31 @@ if($(window).width()<768){
 	})
 	
 }
+function checkstick(){
+	console.log('happened');
+	var hT = $('#gotcha').offset().top,
+       hH = $('#g').outerHeight(),
+       wH = $(window).height(),
+       wS = $(this).scrollTop();
+       console.log(hT);
+       console.log(hH);
+       console.log(wH); 
+       console.log(wS);
+   if (wS > hT){	
+   		console.log('add');
+      	$('#nav2').addClass('getstuck');
+      	$('#bs-example-navbar-collapse-2,#search').addClass('getstuck1');
+   }
+   else{
+   		$('#nav2').removeClass('getstuck');
+   		$('#bs-example-navbar-collapse-2,#search').removeClass('getstuck1');
+   		
+   	   }
+}
+var scrollTimeout = null;
+$(window).scroll(function(){
+    if (scrollTimeout) clearTimeout(scrollTimeout);
+    scrollTimeout = setTimeout(function(){checkstick()},1);
+});
+
 
